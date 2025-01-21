83cf419ba4a890f0838eedf547eed55f2858dc19
sagar-singh11 <sagar.mytechhandle@gmail.com>
2025-01-21 22:41:07 +0530

    added a new var age

--- first.py
+++ first.py
@@ -2,3 +2,9 @@
 
 name = input("Enter your name : ")
 print("Hello " + name + "!")
+age = int(input("Enter age: "))
+
+if(age>18):
+    print("You can vote")
+else:
+    print("You can not vote!")    
